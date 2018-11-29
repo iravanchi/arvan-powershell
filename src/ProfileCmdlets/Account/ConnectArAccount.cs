522cc522d15e1b646bde89b66b6c6a526a0300e4
Hamed Iravanchi <iravanchi@gmail.com>
2018-11-29 18:54:15 +0330

    Add Persistence option, bearer token and username/password option (not yet implemented) to Connect-ArAccount Cmdlet

--- src/ProfileCmdlets/Account/ConnectArAccount.cs
+++ src/ProfileCmdlets/Account/ConnectArAccount.cs
@@ -1,25 +1,65 @@
+using System;
 using System.Management.Automation;
+using System.Security;
 using Arvan.PowerShell.Context.Authentication;
 
 namespace Arvan.PowerShell.ProfileCmdlets.Account
 {
     [OutputType(typeof(string))]
-    [Cmdlet(VerbsCommunications.Connect, "ArAccount", SupportsShouldProcess = true)]
+    [Cmdlet(VerbsCommunications.Connect, "ArAccount", SupportsShouldProcess = true, DefaultParameterSetName = "ApiKey")]
     public class ConnectArAccount : Cmdlet
     {
         [ValidateNotNullOrEmpty]
-        [Parameter(Mandatory = true, HelpMessage = "The API Key connected to the account, provided by Arvan")]
+        [Parameter(Mandatory = true, ParameterSetName = "ApiKey", HelpMessage = "The API Key connected to the account, provided by Arvan")]
         public string ApiKey { get; set; }
+
+        [ValidateNotNullOrEmpty]
+        [Parameter(Mandatory = true, ParameterSetName = "Bearer")]
+        public string BearerToken { get; set; }
+
+        [ValidateNotNullOrEmpty]
+        [Parameter(Mandatory = true, ParameterSetName = "Username")]
+        public string Username { get; set; }
+
+        [ValidateNotNullOrEmpty]
+        [Parameter(Mandatory = true, ParameterSetName = "Username")]
+        public SecureString Password { get; set; }
+
+        private bool _persistent;
+
+        [Parameter]
+        public SwitchParameter Persistent
+        {
+            get => _persistent;
+            set => _persistent = value;
+        }
 
         protected override void ProcessRecord()
         {
+            if (!string.IsNullOrWhiteSpace(Username))
+            {
+                throw new NotImplementedException("Using username and password login is not supported yet.");                
+            }
+            
             if (!ShouldProcess("account", "set"))
                 return;
-            
-            AuthenticationContext.PersistentContext = new AuthenticationContextEntity
+
+            if (_persistent)
+            {
+                AuthenticationContext.PersistentContext = BuildAuthenticationContextEntity();
+            }
+            else
+            {
+                AuthenticationContext.InMemoryContext = BuildAuthenticationContextEntity();
+            }
+        }
+
+        private AuthenticationContextEntity BuildAuthenticationContextEntity()
+        {
+            return new AuthenticationContextEntity
             {
                 ApiKey = ApiKey,
-                BearerToken = null
+                BearerToken = BearerToken
             };
         }
     }
